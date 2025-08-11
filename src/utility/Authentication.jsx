625747ac1f20d1ef7cc39aa77114b7c0a032abdd
Tasnim Ayan <tasnimayan22@gmail.com>
2025-08-12 00:10:16 +0600

    build error due to SellerStore

--- src/utility/Authentication.jsx
+++ src/utility/Authentication.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import SellerStore from '../store/SellerStore.js';
+import Cookies from 'js-cookie';
 
 // This is to control the authenticated routing. No unauthorized user can use defined routes under this component on MainComponent
-const PrivateRoute = () => {
-  const { isLogin } = SellerStore();
-  const isAuthenticated = isLogin();
+const isLoggedIn = () => {
+  return !!Cookies.get('seller');
+};
 
+const PrivateRoute = () => {
+  const isAuthenticated = isLoggedIn();
   return isAuthenticated ? <Outlet /> : <Navigate to="/seller/login"></Navigate>;
 };
 
